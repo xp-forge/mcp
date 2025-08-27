c3adf6ab34584bfb772ff88594da88dc4682ec22
Timm Friebe <thekid@thekid.de>
2025-08-27 20:55:56 +0200

    Add parameters to input schema

--- src/main/php/io/modelcontextprotocol/server/InstanceDelegate.class.php
+++ src/main/php/io/modelcontextprotocol/server/InstanceDelegate.class.php
@@ -21,12 +21,25 @@ class InstanceDelegate {
     $tools= [];
     foreach (Reflection::type($this->instance)->methods() as $name => $method) {
       if ($method->annotation(Tool::class)) {
+
+        // Use annotated parameters if possible
+        $properties= $required= [];
+        foreach ($method->parameters() as $param => $reflect) {
+          $annotations= $reflect->annotations();
+          if ($annotation= $annotations->type(Param::class)) {
+            $properties[$param]= $annotation->newInstance()->schema();
+          } else {
+            $properties[$param]= ['type' => 'string', 'description' => ucfirst($param)];
+          }
+          $reflect->optional() || $required[]= $param;
+        }
+
         $tools[]= [
           'name'        => $name,
           'description' => $method->comment() ?? null,
           'inputSchema' => [
             'type'       => 'object',
-            'properties' => (object)[],
+            'properties' => $properties ?: (object)[],
             'required'   => [],
           ],
         ];
