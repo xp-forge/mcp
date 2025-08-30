ebaa520208d662a34c4314041b506741e37d2ef2
Timm Friebe <thekid@thekid.de>
2025-08-30 23:59:28 +0200

    Simplify JsonRpcTest

--- src/test/php/io/modelcontextprotocol/unittest/JsonRpcTest.class.php
+++ src/test/php/io/modelcontextprotocol/unittest/JsonRpcTest.class.php
@@ -1,48 +1,67 @@
 <?php namespace io\modelcontextprotocol\unittest;
 
 use io\modelcontextprotocol\server\JsonRpc;
-use lang\FormatException;
+use lang\{FormatException, IllegalAccessException};
 use test\{Assert, Expect, Test};
 use web\io\{TestInput, TestOutput};
 use web\{Request, Response};
 
 class JsonRpcTest {
 
-  #[Test]
-  public function handle() {
-    $request= new Request(new TestInput(
-      'POST',
-      '/mcp',
-      ['Content-Type' => 'application/json'],
-      '{"jsonrpc":"2.0","id":"1","method":"initialize"}'
-    ));
+  /** Handle fixture and return body */
+  private function handle($fixture, $payload) {
+    $request= new Request(new TestInput('POST', '/mcp', ['Content-Type' => 'application/json'], $payload));
     $response= new Response(new TestOutput());
-    (new JsonRpc(['initialize' => fn() => true]))->handle($request, $response);
+    foreach ($fixture->handle($request, $response) ?? [] as $_) { }
 
+    // Parse JSON from chunked encoding
+    return preg_match('/\r\n(.+)\r\n/', $response->output()->body(), $matches)
+      ? $matches[1]
+      : null
+    ;
+  }
+
+  #[Test]
+  public function handle_method() {
+    $answer= $this->handle(
+      new JsonRpc(['initialize' => fn() => true]),
+      '{"jsonrpc":"2.0","id":"1","method":"initialize"}'
+    );
     Assert::equals(
-      "28\r\n".
-      '{"jsonrpc":"2.0","id":"1","result":true}'.
-      "\r\n0\r\n\r\n",
-      $response->output()->body()
+      '{"jsonrpc":"2.0","id":"1","result":true}',
+      $answer
+    );
+  }
+
+  #[Test]
+  public function non_existant_handler() {
+    $answer= $this->handle(
+      new JsonRpc([]),
+      '{"jsonrpc":"2.0","id":"1","method":"nonexistant"}'
+    );
+    Assert::equals(
+      '{"jsonrpc":"2.0","id":"1","error":{"code":-32601,"message":"nonexistant"}}',
+      $answer
+    );
+  }
+
+  #[Test]
+  public function errors_raised_by_handler() {
+    $answer= $this->handle(
+      new JsonRpc(['throw' => function() { throw new IllegalAccessException('Test'); }]),
+      '{"jsonrpc":"2.0","id":"1","method":"throw"}'
+    );
+    Assert::equals(
+      '{"jsonrpc":"2.0","id":"1","error":{"code":-32600,"message":"Test"}}',
+      $answer
     );
   }
 
   #[Test]
   public function handle_malformed_json() {
-    $request= new Request(new TestInput(
-      'POST',
-      '/mcp',
-      ['Content-Type' => 'application/json'],
-      'not.json'
-    ));
-    $response= new Response(new TestOutput());
-    (new JsonRpc(['initialize' => fn() => true]))->handle($request, $response);
-
     Assert::equals(
-      "6d\r\n".
-      '{"jsonrpc":"2.0","id":null,"error":{"code":-32700,"message":"Unexpected token [\"not.json\"] reading value"}}'.
-      "\r\n0\r\n\r\n",
-      $response->output()->body()
+      '{"jsonrpc":"2.0","id":null,"error":{"code":-32700,"message":"Unexpected token [\"not.json\"] reading value"}}',
+      $this->handle(new JsonRpc([]), 'not.json')
     );
   }
 
