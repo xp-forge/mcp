087f12413b684a0e7e830180a2b5bdf24c52e17f
Timm Friebe <thekid@thekid.de>
2025-08-30 12:18:27 +0200

    Test instantiation function

--- src/test/php/io/modelcontextprotocol/unittest/ImplementationsInTest.class.php
+++ src/test/php/io/modelcontextprotocol/unittest/ImplementationsInTest.class.php
@@ -4,12 +4,25 @@ use io\modelcontextprotocol\server\ImplementationsIn;
 use test\{Assert, Test};
 
 class ImplementationsInTest extends DelegatesTest {
+  const PACKAGE= 'io.modelcontextprotocol.unittest';
+
+  #[Test]
+  public function can_supply_instantation_function() {
+    $created= [];
+    $impl= new ImplementationsIn(self::PACKAGE, function($class) use(&$created) {
+      $created[]= $class->literal();
+      return $class->newInstance();
+    });
+
+    sort($created);
+    Assert::equals([Calculator::class, Greetings::class], $created);
+  }
 
   #[Test]
   public function tools() {
     Assert::equals(
       [self::CALCULATOR],
-      [...(new ImplementationsIn('io.modelcontextprotocol.unittest'))->tools()]
+      [...(new ImplementationsIn(self::PACKAGE))->tools()]
     );
   }
 
@@ -17,7 +30,7 @@ class ImplementationsInTest extends DelegatesTest {
   public function prompts() {
     Assert::equals(
       [self::GREETINGS],
-      [...(new ImplementationsIn('io.modelcontextprotocol.unittest'))->prompts()]
+      [...(new ImplementationsIn(self::PACKAGE))->prompts()]
     );
   }
 }
