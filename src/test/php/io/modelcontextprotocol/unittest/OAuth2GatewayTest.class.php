1fdfaa2a6003b64ada7c73ada7e935138f0e36eb
Timm Friebe <thekid@thekid.de>
2026-01-03 15:44:51 +0100

    Add test for issuing tokens via /oauth/token

--- src/test/php/io/modelcontextprotocol/unittest/OAuth2GatewayTest.class.php
+++ src/test/php/io/modelcontextprotocol/unittest/OAuth2GatewayTest.class.php
@@ -35,6 +35,12 @@ class OAuth2GatewayTest {
   /** Tokens fixture */
   private function tokens() {
     return new class() extends Tokens {
+
+      public function issue($issuer, $audience, $session) {
+        $session->destroy();
+        return OAuth2GatewayTest::VALID_TOKEN;
+      }
+
       public function use($token) {
         return OAuth2GatewayTest::VALID_TOKEN === $token ? OAuth2GatewayTest::USER : null;
       }
@@ -51,21 +57,17 @@ class OAuth2GatewayTest {
     };
   }
 
-  /** Builds a RFC3986 conformant query string*/
-  private function query(array $parameters): string {
-    return http_build_query($parameters, PHP_QUERY_RFC3986);
-  }
-
   /**
    * Invokes the handler function
    *
    * @param  function(web.Request, web.Response): var $handler
-   * @param  string $uri
+   * @param  var[] $request
    * @param  [:var] $headers
    * @return web.Response
    */
-  private function handle($handler, $uri, $headers= []) {
-    $request= new Request(new TestInput('GET', $uri, $headers));
+  private function handle($handler, $request, $headers= []) {
+    [$method, $uri, $params]= $request;
+    $request= new Request(new TestInput($method, $uri.'?'.http_build_query($params, PHP_QUERY_RFC3986), $headers));
     $response= new Response(new TestOutput());
 
     foreach ($handler($request, $response) ?? [] as $_) { }
@@ -88,7 +90,7 @@ class OAuth2GatewayTest {
   #[Test]
   public function metadata() {
     $gateway= new OAuth2Gateway('/oauth', $this->clients(), $this->tokens());
-    $response= $this->handle($gateway->metadata(), '/', ['Host' => 'test']);
+    $response= $this->handle($gateway->metadata(), ['GET', '/', []], ['Host' => 'test']);
 
     Assert::equals(200, $response->status());
     Assert::equals(
@@ -112,7 +114,7 @@ class OAuth2GatewayTest {
     $handler= $gateway->authenticate(function($request, $response) use(&$authenticated) {
       $authenticated= $request->value('user');
     });
-    $response= $this->handle($handler, '/', ['Authorization' => 'Bearer '.self::VALID_TOKEN]);
+    $response= $this->handle($handler, ['GET', '/', []], ['Authorization' => 'Bearer '.self::VALID_TOKEN]);
 
     Assert::equals(200, $response->status());
     Assert::equals(self::USER, $authenticated);
@@ -124,7 +126,7 @@ class OAuth2GatewayTest {
     $handler= $gateway->authenticate(function($request, $response) {
       throw new IllegalStateException('Unreachable');
     });
-    $response= $this->handle($handler, '/', $headers);
+    $response= $this->handle($handler, ['GET', '/', []], $headers);
 
     Assert::equals(401, $response->status());
     Assert::equals(['WWW-Authenticate' => 'Bearer'], $response->headers());
@@ -132,14 +134,15 @@ class OAuth2GatewayTest {
 
   #[Test]
   public function authorize_redirection() {
-    $sessions= new ForTesting();
     $gateway= new OAuth2Gateway('/oauth', $this->clients(), $this->tokens());
-    $handler= $gateway->flow($this->auth(), $sessions);
-    $response= $this->handle($handler, '/oauth/authorize?'.$this->query([
-      'client_id'    => self::VALID_CLIENT,
-      'redirect_uri' => self::VALID_REDIRECT,
-      'state'        => 'test-state'
-    ]));
+    $handler= $gateway->flow($this->auth(), new ForTesting());
+    $response= $this->handle($handler, ['GET', '/oauth/authorize', [
+      'client_id'             => self::VALID_CLIENT,
+      'redirect_uri'          => self::VALID_REDIRECT,
+      'state'                 => 'test-state',
+      'code_challenge'        => 'test-challenge',
+      'code_challenge_method' => 'S256',
+    ]]);
     $location= new URI($response->headers()['Location']);
 
     Assert::equals(302, $response->status());
@@ -149,14 +152,15 @@ class OAuth2GatewayTest {
 
   #[Test]
   public function cannot_authorize_invalid_redirect() {
-    $sessions= new ForTesting();
     $gateway= new OAuth2Gateway('/oauth', $this->clients(), $this->tokens());
-    $handler= $gateway->flow($this->auth(), $sessions);
-    $response= $this->handle($handler, '/oauth/authorize?'.$this->query([
-      'client_id'    => self::VALID_CLIENT,
-      'redirect_uri' => 'http://example.com',
-      'state'        => 'test-state'
-    ]));
+    $handler= $gateway->flow($this->auth(), new ForTesting());
+    $response= $this->handle($handler, ['GET', '/oauth/authorize', [
+      'client_id'             => self::VALID_CLIENT,
+      'redirect_uri'          => 'http://example.com',
+      'state'                 => 'test-state',
+      'code_challenge'        => 'test-challenge',
+      'code_challenge_method' => 'S256',
+    ]]);
 
     Assert::equals(400, $response->status());
     Assert::matches('/Cannot authorize client test-client/', $response->output()->body());
@@ -164,16 +168,48 @@ class OAuth2GatewayTest {
 
   #[Test]
   public function cannot_authorize_unknown_client() {
-    $sessions= new ForTesting();
     $gateway= new OAuth2Gateway('/oauth', $this->clients(), $this->tokens());
-    $handler= $gateway->flow($this->auth(), $sessions);
-    $response= $this->handle($handler, '/oauth/authorize?'.$this->query([
-      'client_id'    => 'invalid-client',
-      'redirect_uri' => self::VALID_REDIRECT,
-      'state'        => 'test-state'
-    ]));
+    $handler= $gateway->flow($this->auth(), new ForTesting());
+    $response= $this->handle($handler, ['GET', '/oauth/authorize', [
+      'client_id'             => 'invalid-client',
+      'redirect_uri'          => self::VALID_REDIRECT,
+      'state'                 => 'test-state',
+      'code_challenge'        => 'test-challenge',
+      'code_challenge_method' => 'S256',
+    ]]);
 
     Assert::equals(400, $response->status());
     Assert::matches('/Cannot authorize client invalid-client/', $response->output()->body());
+  }
+
+  #[Test]
+  public function token_issued() {
+    $challenge= 'test-challenge';
+    $gateway= new OAuth2Gateway('/oauth', $this->clients(), $this->tokens());
+    $handler= $gateway->flow($this->auth(), new ForTesting());
+
+    // Step 1: Authorize
+    $response= $this->handle($handler, ['GET', '/oauth/authorize', [
+      'client_id'             => self::VALID_CLIENT,
+      'redirect_uri'          => self::VALID_REDIRECT,
+      'state'                 => 'test-state',
+      'code_challenge'        => 'Xuq1l4Pllrvf6AJ2BfBwnQFQKBK7dnKAbolZ3zvWFlw', // base64(sha256(challeng))
+      'code_challenge_method' => 'S256',
+    ]]);
+    $location= new URI($response->headers()['Location']);
+
+    // Step 2: Fetch token
+    $response= $this->handle($handler, ['POST', '/oauth/token', [
+      'client_id'             => self::VALID_CLIENT,
+      'redirect_uri'          => self::VALID_REDIRECT,
+      'grant_type'            => 'authorization_code',
+      'code'                  => $location->param('code'),
+      'code_verifier'         => $challenge,
+    ]]);
+
+    Assert::equals(
+      ['token_type' => 'Bearer', 'access_token' => self::VALID_TOKEN],
+      json_decode($response->output()->body(), true)
+    );
   }
 }
